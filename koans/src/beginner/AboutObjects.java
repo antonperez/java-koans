7725b3c1e3c9ed4faffd98d012167b2a7806dbae
Anton Perez <anton.perez@ticketmaster.com>
2015-04-03 17:33:24 -0700

    Finished strings, objects, and loops modules.

--- koans/src/beginner/AboutObjects.java
+++ koans/src/beginner/AboutObjects.java
@@ -13,7 +13,7 @@ public class AboutObjects {
 
 	@Koan
 	public void newObjectInstancesCanBeCreatedDirectly() {
-		assertEquals(new Object() instanceof Object, __);
+		assertEquals(new Object() instanceof Object, true);
 	}
 
 	@Koan
@@ -21,8 +21,10 @@ public class AboutObjects {
 		class Foo {}
 
 		Class<?>[] ancestors = getAncestors(new Foo());
-		assertEquals(ancestors[0], __);
-		assertEquals(ancestors[1], __);
+		assertEquals(ancestors[0], Foo.class);
+		assertEquals(ancestors[1], Object.class);
+		// nothing in index 1, so it just goes to Object, which is what everything, except primitives,
+		// inherits from in Java
 	}
 
 	@Koan
@@ -30,7 +32,8 @@ public class AboutObjects {
 		Object object = new Object();
 		// TODO: Why is it best practice to ALWAYS override toString?
 		String expectedToString = MessageFormat.format("{0}@{1}", Object.class.getName(), Integer.toHexString(object.hashCode()));
-		assertEquals(expectedToString, __); // hint: object.toString()
+		assertEquals(expectedToString, object.toString()); // hint: object.toString()
+		// Object's toString() prints out name + @ + hashcode
 	}
 
 	@Koan
@@ -41,13 +44,15 @@ public class AboutObjects {
 				return string;
 			}
 		};
-		assertEquals(string + object, __);
+		assertEquals(string + object, "haha");
 	}
 
 	@Koan
 	public void toStringIsTestedForNullWhenInvokedImplicitly() {
 		String string = "string";
-		assertEquals(string + null, __);
+		assertEquals(string + null, "stringnull");
+		// + converts null to "null" because the other operand is a string
+		// this basically creates 2 strings into one.
 	}
 
 	private Class<?>[] getAncestors(Object object) {
